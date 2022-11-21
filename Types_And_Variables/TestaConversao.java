fc0d479a7426de47c3d8a49a61082f6adef22f7f
Erik Rothman <git.erikcr@gmail.com>
2022-11-21 18:11:54 -0300

    Understanding typecast tool

---
+++ Types_And_Variables/TestaConversao.java
@@ -0,0 +1,22 @@
+package Types_And_Variables;
+
+/**
+ * TestaConversao
+ */
+public class TestaConversao {
+
+    public static void main(String[] args) {
+        
+        double salario = 100.50;
+        int valor = (int) salario;
+
+        System.out.println("Valor = " + valor);
+
+        long numeroGrande = 1652552555522455L;
+        short valorPequeno = 2131;
+        byte b = 127;
+
+        float f = 3.4f;
+
+    }
+}
